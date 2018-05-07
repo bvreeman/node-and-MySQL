ebb8231846ec003f310c29f47743a8a9831991d7
bvreeman <bvreeman@gmail.com>
2018-05-06 19:09:36 -0500

    finished the bamazonmanager app. Would like to add some more refinements and make it into a more useable tool if I can figure out how to make it so the connection end would work in between sections of the program

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 require('console.table');
+require('events').EventEmitter.prototype._maxListeners = 100;
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -13,6 +14,13 @@ const connection = mysql.createConnection({
   password: '',
   database: 'bamazon_DB',
 });
+
+function quitApplication() {
+  console.log('    Have a nice day!');
+  console.log('\n<----------------------->\n');
+
+  connection.end();
+}
 
 function itemInquiry() {
   inquirer
@@ -71,6 +79,46 @@ itemInquiry();
 function forSale() {
   connection.connect(function() {
     connection.query('SELECT * FROM products', function(err, results) {
+      if (err) throw err;
+      // console.log(results);
+      console.log('\n<----------------------->\n');
+      console.log('These are the items currently for sale \n');
+      console.table(results);
+      console.log('\n<----------------------->\n');
+      // itemInquiry();
+      quitApplication();
+    });
+  });
+}
+
+function lowInv() {
+  connection.connect(function() {
+    connection.query('SELECT * FROM products', function(err, results) {
+      if (err) {
+        throw err;
+      } else {
+        const lowInventoryArray = [];
+        for (let i = 0; i < results.length; i++) {
+        //   console.log(results[i].stock_quantity);
+          if (results[i].stock_quantity <= 5) {
+            lowInventoryArray.push(results[i]);
+            // itemInquiry();
+          }
+        }
+        console.log('\n');
+        console.table(lowInventoryArray);
+        console.log('\n');
+        console.log('\n<----------------------->\n');
+        quitApplication();
+      }
+    });
+  });
+}
+
+function addInv() {
+//   console.log('add inventory app');
+  connection.connect(function() {
+    connection.query('SELECT * FROM products', function(err, results) {
       if (err) {
         throw err;
       } else {
@@ -79,24 +127,87 @@ function forSale() {
         console.log('These are the items currently for sale \n');
         console.table(results);
         console.log('\n<----------------------->\n');
-        itemInquiry();
+        // itemInquiry();
       }
     });
   });
-}
+  inquirer
+    .prompt([
 
-function lowInv() {
-  console.log('low inventory app');
-}
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Which item would you like to add inventory to?\n',
+        validate: function(value) {
+          if (!isNaN(value)) {
+            return true;
+          }
+          return false;
+        },
+      },
+      {
+        type: 'input',
+        name: 'quantity',
+        message: 'How many of these items would you like to add? \n',
+        validate: function(value) {
+          if (!isNaN(value)) {
+            return true;
+          }
+          return false;
+        },
+      },
 
-function addInv() {
-  console.log('add inventory app');
+    ]).then(function(answer) {
+      connection.query('SELECT * FROM products WHERE ?', [{ item_id: answer.id }], function(err, res) {
+        if (err) throw err;
+        const currentQuantity = res[0].stock_quantity;
+        const newQuantity = parseInt(currentQuantity) + parseInt(answer.quantity);
+        console.log('\n<------------------------->\n');
+        connection.query('UPDATE products SET stock_quantity=? WHERE item_id=?', [newQuantity, answer.id]);
+        connection.query('SELECT * FROM products', function(error, results) {
+          console.log('Inventory available for next purchase:\n');
+          console.table(results);
+          //   itemInquiry();
+          console.log('\n<----------------------->\n');
+          console.log('Inventory has been updated\n');
+          quitApplication();
+        });
+      });
+    });
 }
 
 function addNewProduct() {
-  console.log('add new product app');
-}
-
-function quitApplication() {
-  connection.end();
+//   console.log('add new product app');
+  connection.query('SELECT * FROM products', function(error, results) {
+    if (error) throw error;
+    inquirer.prompt([{
+      name: 'name',
+      type: 'input',
+      message: 'What is the name of the item?',
+    }, {
+      name: 'department',
+      type: 'input',
+      message: 'What is the department of the item?',
+    }, {
+      name: 'price',
+      type: 'input',
+      message: 'What is the price of the item?',
+    }, {
+      name: 'stock',
+      type: 'input',
+      message: 'What is the stock quantity of the item',
+    }]).then(function(answer) {
+      connection.query('INSERT INTO products SET ?', {
+        product_name: answer.name,
+        department_name: answer.department,
+        price: answer.price,
+        stock_quantity: answer.stock,
+      }, function(err) {
+        if (err) throw err;
+        console.log('\n<----------------------->\n');
+        console.log('Item added');
+        quitApplication();
+      });
+    });
+  });
 }
